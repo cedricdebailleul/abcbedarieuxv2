36e96f66177b36ca9941601b39601b166190f39d
cedric debailleul <cedric.debailleul62@gmail.com>
2026-01-07 17:04:24 +0100

    feat: allow images from images.abcbedarieux.com

--- next.config.ts
+++ next.config.ts
@@ -120,6 +120,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.abcbedarieux.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
